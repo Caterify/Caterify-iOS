9b839b51c0ca8471cec2c4bd9fe3966a8a773838
Farrel Najib Anshary <farrelnajib@gmail.com>
2021-11-21 16:02:56 +0700

    Bikin networking schedule

--- Caterify/Networking/Interface/Interface+Schedule.swift
+++ Caterify/Networking/Interface/Interface+Schedule.swift
@@ -7,15 +7,18 @@
 
 import Foundation
 import Combine
+import SwiftUI
 
 // Schedule Interface Functions
 protocol ScheduleInterface: Service {
     func getScheduleInRange(id: Int, start: String, end: String) -> AnyPublisher<CABaseResponse<RangedScheduleBaseResponse>, CABaseErrorModel>
+    func getScheduleOfCatering(date: String) -> AnyPublisher<CABaseResponse<ScheduleBaseResponse>, CABaseErrorModel>
 }
 
 // Schedule Request Enum
 enum ScheduleDescription {
     case getScheduleInRange(id: Int, start: String, end: String)
+    case getScheduleOfCatering(date: String)
 }
 
 extension ScheduleDescription: NetworkDescription {
@@ -23,6 +26,8 @@ extension ScheduleDescription: NetworkDescription {
         switch self {
         case .getScheduleInRange(let id, _, _):
             return "/api/public/schedules/catering/\(id)/range"
+        case .getScheduleOfCatering:
+            return "/api/private/schedules/date"
         }
     }
     
@@ -40,6 +45,22 @@ extension ScheduleDescription: NetworkDescription {
                 "api_key": Constants.ServerEnvironment.apiKey,
                 "start": start,
                 "end": end
+            ]
+        case .getScheduleOfCatering(let date):
+            return [
+                "api_key": Constants.ServerEnvironment.apiKey,
+                "date": date
+            ]
+        }
+    }
+    
+    var headers: [String : String]? {
+        @AppStorage("token") var token: String = ""
+        switch self {
+        default:
+            return [
+                "Authorization": "Bearer \(token)",
+                "Accept": "application/json"
             ]
         }
     }
